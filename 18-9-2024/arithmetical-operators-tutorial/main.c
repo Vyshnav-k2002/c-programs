26ac1ff4c68488004ecddfbc0aade24de3d33fbc
Vyshnav-k2002 <144435309+Vyshnav-k2002@users.noreply.github.com>
2024-09-19 14:03:54 +0530

    Add files via upload

---
+++ 18-9-2024/arithmetical-operators-tutorial/main.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    int b=5;
+    int sum=a+b;
+    printf("sum of %d and %d is %d\n",a,b,sum);
+    
+  
+    int diff=a-b;
+    printf("difference of %d and %d is %d\n",a,b,diff);
+    
+
+    int mul=a*b;
+    printf("multiple of %d and %d is %d\n",a,b,mul);
+    
+  
+    int div=a/b;
+    printf("division of %d and %d is %d\n",a,b,div);
+    
+  
+    int rem=a%b;
+    printf("reminder while deviding %d and %d is %d\n",a,b,rem);
+    
+    
+
+    return 0;
+}
